2ef4f4420995704717e1b2f918ebcc5139f95a1e
Jakub Bialas <jakub.bialas21@o2.pl>
2024-01-05 11:21:51 +0100

    Finalny wyglad programu z wyborem obliczania pola.

--- Obliczanie pola.c
+++ Obliczanie pola.c
@@ -2,23 +2,38 @@
 #include <math.h>
 float a = 0;
 float b = 0;
-float r=0;
-float obliczanie_pola_kola(float r)
-{ 
-return(M_PI * pow(r,2));
-}
-float obliczeniePolaProstokata(float a, float b){
-    return (a*b);
-}
-int main() {
-    printf("Podaj pierwszy wymiar prostokata:\t");
-    scanf("%f", &a);
-    printf("Podaj drugi wymiar prostokata:\t");
-    scanf("%f", &b);
-    printf("Pole wynosi:\t%f", obliczeniePolaProstokata(a, b));
-   
-    printf("\nPodaj promien kola:");
+float r = 0;
+int select = 0;
+
+void obliczeniePolaKola(){ 
+    printf("\nPodaj promien kola:\t");
     scanf("%f", &r);
-    printf("Pole kola wynosi:%f", obliczanie_pola_kola(r));
-    return 0;
+    printf("\nPole kola wynosi:%f\n", M_PI*pow(r, 2));
+}
+
+void obliczeniePolaProstokata(){
+    printf("\nPodaj pierwszy wymiar prostokata:\t");
+    scanf("%f", &a);
+    printf("\nPodaj drugi wymiar prostokata:\t");
+    scanf("%f", &b);
+    printf("\nPole prostokata wynosi:\t%f\n", (a*b));
+}
+
+int main() {
+	while (1){
+		printf("\n1-Pole prostokanta\n2-Pole kola\n3-Wyjscie z programu\n");
+		scanf("%d", &select);
+		switch(select){
+			case 1:
+				obliczeniePolaProstokata();
+				break;
+			case 2:
+				obliczeniePolaKola();
+				break;
+			case 3:
+		 		return 0;
+			default:
+				printf("\nNieprawid³owa komenda\n"); 		
+		}
+	}
 }
